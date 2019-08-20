c90a80b9816b8de559e9fb2b4840899697c43b84
Armakuji <draculalovekill@hotmail.com>
2019-08-20 17:32:44 +0700

    add dataBiding

--- app/src/main/java/buu/informatics/s59160575/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160575/aboutme/MainActivity.kt
@@ -8,9 +8,10 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import buu.informatics.s59160575.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
